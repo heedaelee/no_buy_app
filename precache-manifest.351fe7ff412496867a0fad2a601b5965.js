27bdcc116b89a4eba470bcb611ec8b0e25aa6cea
David <adguy72@gmail.com>
2020-04-11 20:19:25 +0900

    Updates

---
+++ precache-manifest.351fe7ff412496867a0fad2a601b5965.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7244ff1f9687bd979d33ad0ea88183a5",
+    "url": "/no_buy_app/index.html"
+  },
+  {
+    "revision": "86362fce0fa0366367d9",
+    "url": "/no_buy_app/static/js/2.200bdeab.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/no_buy_app/static/js/2.200bdeab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89f9781030c9f16af598",
+    "url": "/no_buy_app/static/js/main.ee35b5d3.chunk.js"
+  },
+  {
+    "revision": "d76910a965a95cfb8958",
+    "url": "/no_buy_app/static/js/runtime-main.991eec65.js"
+  }
+]);
